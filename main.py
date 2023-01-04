d39112755a43dedbc7dc82d757244ec4c173ea13
caiosilvestre <scaioimperial@gmail.com>
2023-01-04 20:48:42 -0300

    change directory

---
+++ main.py
@@ -0,0 +1,3 @@
+from pycertify.app import app                                                               
+if __name__ == "__main__":                                             
+    app.run(debug=True)
