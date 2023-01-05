4cab9c2ddebe10ba54c2b939318415a43dbd8390
caiosilvestre <scaioimperial@gmail.com>
2023-01-04 22:29:30 -0300

    ajuste no run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from pycertify.app import create_app
 import os
 
 if(__name__ =='__main__'):
-    port = int(os.getenv('PORT'), 5000)
+    port = int(os.getenv('PORT'), '5000')
     create_app.run(host='0.0.0.0', port = port)
