642250b9852f68a451d5984b6eed3a41ac57eeab
CaioImperial <39228803+caiosilvestre@users.noreply.github.com>
2023-01-24 00:24:18 -0300

    comentando código

--- pycertify/app.py
+++ pycertify/app.py
@@ -3,8 +3,10 @@ from flask import Flask
 from pycertify.blueprints import restapi
 from pycertify.ext import views
 
+#inicia o app em Flask
 app = Flask(__name__)
 
+#chama a modulos api e as páginas
 restapi.init_app(app)
 views.init_app(app)
 #configuration.load_extentions(app)
